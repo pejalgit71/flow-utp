786c95a82ca1162a4f3884bcbddc55bdad05c823
pejalgit71 <160094060+pejalgit71@users.noreply.github.com>
2025-06-24 09:35:18 +0800

    Update STEMCertificationQuiz.py

--- STEMCertificationQuiz.py
+++ STEMCertificationQuiz.py
@@ -9,10 +9,10 @@ SHEET_NAME = "STEM Certification Data"
 def get_gsheet_client():
     scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
     def get_gsheet_client():
-    scope = [
-        "https://spreadsheets.google.com/feeds",
-        "https://www.googleapis.com/auth/drive"
-    ]
+        scope = [
+            "https://spreadsheets.google.com/feeds",
+            "https://www.googleapis.com/auth/drive"
+        ]
     creds_dict = st.secrets["gcp_service_account"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(dict(creds_dict), scope)
     return gspread.authorize(creds)
