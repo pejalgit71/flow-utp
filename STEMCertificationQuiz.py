73577ac9a93b6186324cfc77b0b101f44639897a
pejalgit71 <160094060+pejalgit71@users.noreply.github.com>
2025-07-07 15:39:09 +0800

    Update STEMCertificationQuiz.py

--- STEMCertificationQuiz.py
+++ STEMCertificationQuiz.py
@@ -152,14 +152,14 @@ def admin_question_gui():
 # === Main App ===
 def main():
     st.set_page_config(page_title="UTP STEM Certification Quiz", layout="wide")
+    st.image("UTP.png","MyFLowlab.png")
     st.title("Universiti Teknologi PETRONAS STEM Exploration Assesment")
 
     if "username" not in st.session_state:
         st.session_state["username"] = ""
     st.sidebar.image("MyFLowlab.png")
     st.sidebar.image("UTP.png")
-    st.image("MyFLowlab.png")
-    st.image("UTP.png")
+    
     menu = ["Login", "Sign Up"]
     choice = st.sidebar.selectbox("Menu", menu)
 
